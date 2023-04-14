256db56555949358fb64d456c78f78cd9180ed0b
kurokawa <zjx4299@163.com>
2023-04-14 23:38:14 +0800

    UPD

---
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.atguigu.mybatisplus.mapper;
+
+import com.atguigu.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
